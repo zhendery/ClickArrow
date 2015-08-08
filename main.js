bf4f42b8792827c973087fa349b9de72262dd64f
zhendery <zhendery@qq.com>
2015-08-08 15:58:09 +0800

    最终版
    压缩过了

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 cc.game.onStart = function(){
     cc.view.adjustViewPort(true);
-    cc.view.setDesignResolutionSize(640, 960, cc.ResolutionPolicy.FIXED_WIDTH);
+
+    cc.view.setDesignResolutionSize(640, 960,
+        (cc.director.getWinSize().width>cc.director.getWinSize().height ? 
+        cc.ResolutionPolicy.FIXED_HEIGHT : cc.ResolutionPolicy.FIXED_WIDTH));
+
     cc.view.resizeWithBrowserSize(true);
     //load resources
     cc.LoaderScene.preload(g_resources, function () {
