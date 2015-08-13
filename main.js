2da1c92c767cf074a71ab93791738a7da1e38827
zhendery <zhendery@qq.com>
2015-08-13 18:35:48 +0800

    不知道啥时候又有变动的~反正已经定稿了

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ cc.game.onStart = function(){
     cc.view.adjustViewPort(true);
 
     cc.view.setDesignResolutionSize(640, 960,
-        (cc.director.getWinSize().width>cc.director.getWinSize().height ? 
+        (document.body.clientWidth>document.body.clientHeight ? 
         cc.ResolutionPolicy.FIXED_HEIGHT : cc.ResolutionPolicy.FIXED_WIDTH));
 
     cc.view.resizeWithBrowserSize(true);
